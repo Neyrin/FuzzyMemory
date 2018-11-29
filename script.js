54b3a9aeedfbbe39669ca2220727d65fe2a49621
Neyrin <isa.areschoug@gmail.com>
2018-11-29 17:24:20 +0100

    Fixed card createElement

--- script.js
+++ script.js
@@ -31,27 +31,26 @@ const cardsArray = [{
    'img' : 'Images/invader7.png',
 },
 ];
-let gameGrid = cardsArray.concat(cardsArray) .sort(() => 0.5 - Math.random());;
+let gameBoard = cardsArray
+.concat(cardsArray)
+.sort(() => 0.5 - Math.random());;
 
 const deck = document.getElementById("deck");
-
 const grid = document.createElement("section");
 grid.setAttribute("class", "grid");
-
 deck.appendChild(grid);
 
-gameGrid.forEach(item => {
+gameBoard.forEach(item => {
    const card = document.createElement('div');
    card.classList.add('card');
    card.dataset.name = item.id;
-   card.style.backgroundImage = `url(${item.img})`;
 
   const front = document.createElement('div');
-  front.classList.add('card');
+  front.classList.add('front');
+  card.style.cardValue = `url(${item.img})`;
 
   const back = document.createElement('div');
-  back.classList.add('card');
-  back.style.backgroundImage = `url(Images/space-invaders.png)`;
+  back.classList.add('back');
 
   grid.appendChild(card);
   card.appendChild(front);
