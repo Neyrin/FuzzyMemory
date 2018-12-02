244c2a61f2bf63fc605915b078a69524e3ede9f5
Neyrin <isa.areschoug@gmail.com>
2018-12-02 18:36:23 +0100

    Added functions to flip, match and disable cards

--- script.js
+++ script.js
@@ -31,13 +31,14 @@ const cardsArray = [{
    'img' : 'Images/invader7.png',
 },
 ];
-function startGame() {
 let gameBoard = cardsArray
 .concat(cardsArray)
 .sort(() => 0.5 - Math.random());;
 
-const deck = document.getElementById("deck");
-const grid = document.createElement("section");
+
+
+   const deck = document.getElementById("deck");
+   const grid = document.createElement("section");
 grid.setAttribute("class", "grid");
 deck.appendChild(grid);
 
@@ -58,8 +59,106 @@ gameBoard.forEach(item => {
    card.appendChild(back);
    card.appendChild(front);
 });
-};
 
-window.onload = startGame();
-const button = document.getElementById('button');
+const match = () => {
+   var selected = document.querySelectorAll('.selected');
+   selected.forEach(card => {
+      card.classList.add('match');
+   });
+}
 
+
+/* vv Flips cards, check for match, if matched add class "disabled", if not matched flips cards back vv */
+const cards = document.querySelectorAll('.card');
+
+let selected = false;
+let firstGuess, secondGuess;
+let count = 0;
+
+function flipCard() {
+  this.classList.add('active');
+   if (count < 2){
+      count++;
+      if (!selected) {
+         selected = true;
+         firstGuess = this;
+         return;
+      }
+
+   secondGuess = this;
+   selected = false;
+  
+   
+   checkMatch()
+   }
+}
+function checkMatch() {
+   if (firstGuess.dataset.name === secondGuess.dataset.name) {
+      disableCards();
+      return;
+   }
+   
+   restoreCards();
+}
+
+function disableCards() {
+   firstGuess.removeEventListener('click', flipCard);
+   firstGuess.classList.add('disabled');
+   secondGuess.removeEventListener('click', flipCard);
+   secondGuess.classList.add('disabled');
+}
+
+function restoreCards() {
+   setTimeout(() => {
+      firstGuess.classList.remove('active');
+      secondGuess.classList.remove('active');
+   }, 1500);
+   
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+/* ^^ Flips cards, check for match, if matched add class "disabled", if not matched flips cards back ^^ */
+
+
+
+
+
+
+
+
+
+
+/* function startGame() {
+   game = createGame();
+   for (var i = 0; i < gameBoard.length; i++){
+      deck.innerHTML = "";
+      [].forEach.call(card, function(item) {
+         deck.appendChild(item);
+      });
+      card[i].classList.remove("show", "open", "match", "disabled");
+   }
+}; */
+
+/* window.onload = createGame(); 
+   /* grid.addEventListener('click', function (event) {
+      
+      let clicked = event.target;
+      
+      if (clicked.nodeName === 'SECTION') { return; }
+      
+      if (count < 2) {
+         count++;
+         if (count === 1) {
+            firstGuess = clicked.dataset. name;
+            clicked.classList.add('selected');
+         } else {
+            secondGuess = clicked.dataset.name;
+            clicked.classList.add('selected');
+         }
+         if (firstGuess !== '' && secondGuess !== '') {
+            if (firstGuess === secondGuess) {
+               match();
+            }
+         }   
+      }
+   }); */
