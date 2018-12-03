4e90aa1a0ea0158083df370282e16666348a6bce
Neyrin <isa.areschoug@gmail.com>
2018-12-03 11:36:07 +0100

    Created restart functionality for button

--- script.js
+++ script.js
@@ -34,11 +34,14 @@ const cardsArray = [{
 let gameBoard = cardsArray
 .concat(cardsArray)
 .sort(() => 0.5 - Math.random());;
+let deck;
+let card;
+let grid;
 
 
-
-   const deck = document.getElementById("deck");
-   const grid = document.createElement("section");
+function startGame(gameBoard) {
+const deck = document.getElementById("deck");
+const grid = document.createElement("section");
 grid.setAttribute("class", "grid");
 deck.appendChild(grid);
 
@@ -46,20 +49,19 @@ gameBoard.forEach(item => {
    const card = document.createElement('div');
    card.classList.add('card');
    card.dataset.name = item.id;
-   
+
    const back = document.createElement('div');
    back.classList.add('back');
-   
+
    const front = document.createElement('div');
    front.classList.add('front');
-   card.style.cardValue = `url(${item.img})`;
-   
-   
+   card.style.backgroundImage = `url(${item.img})`;
+
+
    grid.appendChild(card);
    card.appendChild(back);
    card.appendChild(front);
 });
-
 
 /* vv Flips cards, check for match, if matched add class "disabled", if not matched flips cards back vv */
 const cards = document.querySelectorAll('.card');
@@ -81,7 +83,7 @@ function flipCard() {
    }
 
    secondGuess = this;
-  
+
    checkMatch()
    }
 
@@ -91,7 +93,7 @@ function checkMatch() {
       disableCards();
       return;
    }
-   
+
    restoreCards();
 }
 
@@ -101,7 +103,7 @@ function disableCards() {
    firstGuess.classList.add('disabled');
    secondGuess.removeEventListener('click', flipCard);
    secondGuess.classList.add('disabled');
-   
+
    resetBoard();
 }
 
@@ -113,7 +115,7 @@ function restoreCards() {
       secondGuess.classList.remove('active');
       resetBoard();
    }, 1500);
-   
+
 }
 
 /* Resets board from previous selection to allow all cards (not matched/disabled cards) to be selected again */
@@ -125,6 +127,24 @@ function resetBoard() {
 cards.forEach(card => card.addEventListener('click', flipCard));
 /* ^^ Flips cards, check for match, if matched add class "disabled", if not matched flips cards back ^^ */
 
+};
+
+window.addEventListener("load", () => {
+   startGame(gameBoard);
+});
+
+function restartGame() {
+   var game = document.getElementById("deck");
+   while (game.firstChild) {
+      game.removeChild(game.firstChild);
+}
+   let gameBoard = cardsArray
+   .concat(cardsArray)
+   .sort(() => 0.5 - Math.random());;
+
+   startGame(gameBoard);
+
+};
 
 
 
@@ -145,13 +165,13 @@ cards.forEach(card => card.addEventListener('click', flipCard));
    }
 }; */
 
-/* window.onload = createGame(); 
+/* window.onload = createGame();
    /* grid.addEventListener('click', function (event) {
-      
+
       let clicked = event.target;
-      
+
       if (clicked.nodeName === 'SECTION') { return; }
-      
+
       if (count < 2) {
          count++;
          if (count === 1) {
@@ -165,6 +185,6 @@ cards.forEach(card => card.addEventListener('click', flipCard));
             if (firstGuess === secondGuess) {
                match();
             }
-         }   
+         }
       }
    }); */
